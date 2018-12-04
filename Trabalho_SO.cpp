a9985c267bdb308d7c0c1435fd6f40e4712ada1c
Gakkou <lucas.soares.avila@gmail.com>
2018-12-04 11:58:20 -0200

    implementa da incialização e os comando "mkdir", "cd .", "cd nome" e ls do sistema FAT.

--- Trabalho_SO.cpp
+++ Trabalho_SO.cpp
@@ -22,6 +22,24 @@ public:
     *data = std::time(nullptr);
     pbloco = pb;
   }
+
+  std::string getNome() {
+    return nome;
+  }
+
+  void lista(int tb) {
+    if(DFT.empty()) {
+      std::cout << "Diretório se encontra vazio.\n";
+    } else {
+      for(auto it = DFT.begin(); it != DFT.end(); it++) {
+        if(it->tipo == 'D') {
+          std::cout << "D " << it->nome << "\n";
+        } else {
+          std::cout << "A " << it->nome << it->tamanho << ((it->tamanho)/tb) << "\n";
+        }
+      }
+    }
+  }
 };
 
 int main() {
@@ -47,7 +65,7 @@ int main() {
 
   // Aloca bitmap
   bitmap = new int[nb];
-  
+
   // Seta para zero
   for(int i = 0; i < nb; i++) {
     bitmap[i] = 0;
@@ -63,11 +81,83 @@ int main() {
     }
 
     // Cria diretório '/'
+    Metadado *aux = new Metadado('D', "/");
+    std::list<Metadado> pilha;
+    pilha.push_back(*aux);
   }
 
   std::getline(entrada, std::cin);
   while(entrada != "quit") {
+    if(flagSA == 1) {
+      if(entrada.compare(0,5,"mkdir") == 0) {
+        entrada.erase(0,6); // Consome o comando
 
+        if(entrada.size() <= 8) {
+          // Nome não sofre corte
+          aux = new Metadado('D', entrada);
+        } else {
+          // Nome muito grande, corta
+          aux = new Metadado('D', entrada.substr(0,8));
+        }
+
+        // Verifica se já existe conteúdo no diretório e se os nomes são diferentes
+        if(pilha.back().DFT.empty()) {
+          pilha.back().DFT.push_back(*aux); // Adiciona ao diretório atual
+          std::cout << "Diretório criado com sucesso.\n"
+        } else {
+          for(auto it = pilha.back().DFT.begin(); it != pilha.back().DFT.end(); it++) {
+            if(it->getNome() == aux->getNome()) {
+              break;
+            }
+          }
+          if(it == pilha.back().DFT.end()) {
+            pilha.back().DFT.push_back(*aux); // Adiciona ao diretório atual
+            std::cout << "Diretório criado com sucesso.\n"
+          }
+          else {
+            std::cout << "ERRO! Já exite Diretório/Arquivo com esse nome.\n";
+          }
+        }
+      }
+
+      if(entrada.compare(0,2,"cd" == 0)) {
+        entrada.erase(0,3); // Consome comando
+
+        if(entrada.compare(0,1,'.') == 0) {
+          if(pilha.back().getNome() == "/") {
+            std::cout << "Usuário já se encontra no diretório raiz.\n";
+          } else {
+            *aux = pilha.back(); // Salva o estado do último diretório
+            pilha.pop_back(); // Sobe um diretório na hierárquia
+            // Atualiza o estado do antigo diretório aberto no novo
+            for(auto it = pilha.back().DFT.begin(); it != pilha.back().DFT.end(); it++) {
+              if(it->getNome() == aux->getNome()) {
+                pilha.back().DFT.erase(it);
+                pilha.back().DFT.push_back(*aux);
+                break;
+              }
+            }
+          }
+        } else {
+          // Verifica se existe diretório com esse nome
+          for(auto it = pilha.back().DFT.begin(); it != pilha.back().DFT.end(); it++) {
+            if(entrada.compare(0,it->getNome().size(),it->getNome()) == 0) {
+              pilha.push_back(*it);
+              break;
+            }
+          }
+          if(it == pilha.back().DFT.end()) {
+            std::cout << "ERRO! Não encontrado diretório com nome " << entrada << "\n";
+          }
+        }
+      }
+
+      if(entrada.compare(0,2,"ls") == 0) {
+        pilha.back().listar(tb);
+      }
+    }
+
+    std::getline(entrada, std::cin);
   }
 
   return 0;
